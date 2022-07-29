bbfc6cf31d04954bec4325b6636d496b2b3af023
Alex <aikozindev@gmail.com>
2022-07-29 20:58:58 +0300

    Готовая верстка окна регистрации

--- app/src/main/java/com/example/messenger/ActivityLogin.java
+++ app/src/main/java/com/example/messenger/ActivityLogin.java
@@ -9,6 +9,6 @@ public class ActivityLogin extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.fragment_registration_get_code);
     }
 }
